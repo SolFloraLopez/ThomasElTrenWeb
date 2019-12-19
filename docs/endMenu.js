8d9660264c3e882d6305c44c2a36439e90b0beea
Will7461 <43720307+Will7461@users.noreply.github.com>
2019-12-19 14:29:44 +0100

    input de texto acabado

--- docs/endMenu.js
+++ docs/endMenu.js
@@ -16,24 +16,37 @@ export default class EndMenu extends Phaser.Scene {
   create()
   {
     this.scene.bringToTop(this);
-    let inputText = document.createElement("INPUT");
-    inputText.setAttribute("type", "text");
-    inputText.setAttribute("placeholder", "Nombre");
-    document.getElementById("section3").appendChild(inputText); 
-    console.log(inputText)
+    this.inputText = document.createElement("input");
+    this.inputText.setAttribute("type", "text");
+    this.inputText.setAttribute("placeholder", "Nombre");
+    this.inputText.setAttribute("maxlenght","3");
+    document.body.appendChild(this.inputText); 
+
+    this.inputBtn = document.createElement("input");
+    this.inputBtn.setAttribute("type", "button");
+    this.inputBtn.setAttribute("id", "saveBtn");
+    this.inputBtn.setAttribute("value", "Guardar récord");
+    document.body.appendChild(this.inputBtn);
+    
+    
 
     this.add.image(0,0,'endBG').setOrigin(0);
-    let menuButton = this.add.image(275,510,'mainMenuBtn').setOrigin(0);
-    this.add.text(110, 269, 'Puntos: '+this.score, { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' ,fontSize: '140px',fill: '#00c2eb'});
+    let menuButton = this.add.image(275,530,'mainMenuBtn').setOrigin(0);
+    this.add.text(110, 230, 'Puntos: '+this.score, { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' ,fontSize: '140px',fill: '#00c2eb'});
+    this.add.text(185, 380, '🡇    Guarda tu récord    🡇 \n🡇    al final de la web    🡇', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' ,fontSize: '60px',fill: '#000000'});
     menuButton.setInteractive();
 
     menuButton.on('pointerup',()=>{
+      this.inputText.parentNode.removeChild(this.inputText);
       this.sound.play('button');
       this.scene.remove('main');
       this.scene.start('menu');
       this.scene.remove(this);
     });
 
+  }
+  myFun() {
+    
   }
   
 }
